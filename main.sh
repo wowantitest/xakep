754afb7fefd120cb6ab5a867c2a9fc49e30f02b1
wowantitest <47903608+wowantitest@users.noreply.github.com>
2020-12-09 17:36:51 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ echo -e "\e[34m
   ███   █▒  █  █░█░   █       █
  ░█ █░  █░ ▓█  █ ░█   ▓▓  █   █
  █▒ ▒█  ▒██▒█  █  ▒█   ███▒   █
-made by @pkgsearch \e[0m"
-echo -e "\e[34m ДУДОС [ python ~/px/DDOS.py ] \e[0m"
-echo -e "\e[34m БОМБЕР [ python ~/px/BOMBER.py ] \e[0m"
+made by @wowantitest \e[0m"
+echo -e "\e[34m DDOS [ python ~/px/DDOS.py ] \e[0m"
+echo -e "\e[34m BOMB [ python ~/px/BOMBER.py ] \e[0m"
 echo -e " >>Введите команду:"
